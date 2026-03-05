9b71c1e3adcd92dac37917b6a053961c351f8f00
Clint Laurence Englis <clintenglis16@gmail.com>
2026-03-05 15:56:36 +0800

    part2_withss

--- app/src/main/java/com/example/mobilecontexttheactivitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/mobilecontexttheactivitylifecycle/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.mobilecontexttheactivitylifecycle;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // FIXED sabotage (no null)
+        TextView counterDisplay = findViewById(R.id.tvDepartment);
+        counterDisplay.setText("0");
 
         if (getSupportActionBar() != null) getSupportActionBar().setTitle("ID: 11500160");
 
