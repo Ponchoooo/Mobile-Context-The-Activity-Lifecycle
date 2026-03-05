0d1be89b85c97725ce125e2d7a53148ef94c85a0
Clint Laurence Englis <clintenglis16@gmail.com>
2026-03-05 15:41:20 +0800

    part_1

--- app/src/main/java/com/example/mobilecontexttheactivitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/mobilecontexttheactivitylifecycle/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.mobilecontexttheactivitylifecycle;
-
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        if (getSupportActionBar() != null) getSupportActionBar().setTitle("ID: 1234567");
+
+        TextView tv = findViewById(R.id.tvDepartment);
+        Button btn = findViewById(R.id.btnChangeName);
+
+        tv.setText("Department of Computer, Information Sciences, and Mathematics");
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                tv.setText("Angie M. Ceniza-Canillo");
+            }
         });
     }
 }
